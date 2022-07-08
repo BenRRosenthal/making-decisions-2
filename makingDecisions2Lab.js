f66f5fff2ae28853864e41f5a7b640373dfee55a
BenRRosenthal <benrrosenthal@gmail.com>
2022-07-07 20:17:27 -0600

    Problems all the way to 10 are complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -126,7 +126,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (let i = letters.length - 1; i >= 0; i--){
+    console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -148,7 +150,19 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch (letterGrade) {
+  case 'A': console.log('The student is doing excellently.')
+  break;
+  case 'B': console.log('The student is doing well.')
+  break;
+  case 'C': console.log('The student is doing alright.')
+  break;
+  case 'D': console.log('The student is not doing very well.')
+  break;
+  case 'F': console.log('The student is failing.')
+  break;
+  default: console.log('Not an eligible grade.')
+}
 
 
 ////////// PROBLEM 11 //////////
